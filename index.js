5ab478ebcaeb0e55596981f839900238da2ed244
Esemdis <christofferthor@msn.com>
2025-06-09 23:51:21 +0200

    Added doppler and fixed redis

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = 3000;
-const { RedisStore } = require("connect-redis");
-const { createClient } = require("redis");
-
-const redisClient = createClient({
-  url: process.env.REDIS_URL,
-});
-redisClient.connect().catch(console.error);
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/users", require("./routes/users"));
--- utils/cache.js
+++ utils/cache.js
@@ -1,27 +1,32 @@
 const { v4: uuidv4 } = require("uuid");
-const { createClient } = require("redis");
-const redisClient = createClient({ url: process.env.REDIS_URL });
-redisClient.connect().catch(console.error);
+const { Redis } = require("ioredis");
+
+const client = new Redis({ url: process.env.REDIS_URL });
 
 function generateCacheKey(prefix) {
   return `${prefix}:${uuidv4()}`;
 }
 async function cacheData({ prefix, data, ttl = 3600 }) {
-  const key = generateCacheKey(prefix);
-  await redisClient.setEx(key, ttl, JSON.stringify(data));
-  return key;
+  try {
+    const key = generateCacheKey(prefix);
+    await client.set(key, JSON.stringify(data), "EX", ttl);
+    return key;
+  } catch (error) {
+    console.error("Error caching data:", error);
+    throw new Error("Failed to cache data");
+  }
 }
 async function getCachedData({ key }) {
-  const data = await redisClient.get(key);
+  const data = await client.get(key);
   return data ? JSON.parse(data) : null;
 }
 async function deleteCachedData({ key }) {
-  await redisClient.del(key);
+  await client.del(key);
 }
 async function clearCache({ prefix }) {
-  const keys = await redisClient.keys(`${prefix}:*`);
+  const keys = await client.keys(`${prefix}:*`);
   if (keys.length > 0) {
-    await redisClient.del(keys);
+    await client.del(keys);
   }
 }
 module.exports = {
