a6775af3a6d5a5857b89b5f4f4a53c6bf78bba47
Esemdis <christofferthor@msn.com>
2026-05-02 23:15:54 +0200

    Added 30 connection limit

--- prisma/client.js
+++ prisma/client.js
@@ -1,6 +1,15 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = global.prisma || new PrismaClient();
+function buildDatasourceUrl() {
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("connection_limit", "30");
+  url.searchParams.set("pool_timeout", "30");
+  return url.toString();
+}
+
+const prisma = global.prisma || new PrismaClient({
+  datasourceUrl: buildDatasourceUrl(),
+});
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
